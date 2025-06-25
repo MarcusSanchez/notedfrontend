3aaa78bec3b1825b8f83fcd8df4a930115dd17dc
Marcus Sanchez <89233973+MarcusSanchez@users.noreply.github.com>
2025-06-25 13:29:31 -0400

    Updated app-build-manifest.json

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/project/src/app/layout\":[\"static/media/4563355e0480477b-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
